b075263b98e36e0d7f86d188ae351320b2ea7c49
Yoshicbtis75 <114589821+Yoshicbtis75@users.noreply.github.com>
2022-10-21 08:59:30 -0500

    Add files via upload

---
+++ Actividad1/html/hola.php
@@ -0,0 +1,24 @@
+<!--
+    description: Saludo.
+    Autor: Joselin Vazquez Frausto.
+    Fecha: 21/10/2022
+-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/estilo.css">
+</head>
+<body>
+    <center>
+        <h1>
+    <?php
+    echo "HOLA A TODOS";
+    ?>
+        </h1>
+    </center>
+</body>
+</html>
